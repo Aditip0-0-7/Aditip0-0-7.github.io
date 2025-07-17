84315953c50056977339187e1ce21f2af0fe934b
Aditip0-0-7 <aditipyomsriken@gmail.com>
2025-07-17 15:40:50 +0700

    Add files via upload

---
+++ Lap.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Products
+SELECT * FROM Employees
+SELECT * FROM EmployeesID,firsname,lastname FROM Employees
+
+SELECT * FROM Employees WHERE City = 'London'
+
+SELECT * FROM Employees 
+WHERE City = 'London'
+
+SELECT City,COuntry 
+FROM Customers
+
+SELECT DiSTINCT City, Country FROM
+ Customers
