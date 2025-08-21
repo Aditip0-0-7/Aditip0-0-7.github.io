e0e48b17b6b5365a59b74e88b6089e7781ac08b8
Aditip0-0-7 <aditipyomsriken@gmail.com>
2025-08-21 15:15:04 +0700

    Update Lad2.sql

--- Lad2.sql
+++ Lad2.sql
@@ -87,4 +87,29 @@ JOIN Products p ON od.ProductID = p.ProductID
 WHERE e.FirstName = 'Nancy'
 ORDER BY p.ProductID
 
+--ต้องาการชื่อบริษัทลูกค้าชื่อ Around the Horn ซื้อสินค้าที่มาจากประเทศอะไรบ้าง
+SELECT DISTINCT s.Country
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+JOIN Products p ON od.ProductID = p.ProductID
+JOIN Suppliers s ON p.SupplierID = s.SupplierID
+WHERE c.CompanyName = 'Around the Horn'
+
+-- บริษัทลูกค้าชื่อ Around the Horn ซื้อสินค้าอะไรบ้าง จำนวนเท่าไหร่
+
+SELECT p.ProductID, p.ProductName, SUM(od.Quantity) AS จำนวนที่ซื้อ
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+JOIN Products p ON od.ProductID = p.ProductID
+WHERE c.CompanyName = 'Around the Horn'
+GROUP BY p.ProductID, p.ProductName
+
+--ต้องการหมายเลขใบสั่งซื้อ ชื่อพนักงาน และยอดขายในใบสั่งซื้อนั้น
+SELECT o.OrderID, firstname, ROUND(SUM(od.Quantity* od.UnitPrice * (1 - od.Discount)),2) TotalCash
+FROM Orders o
+JOIN Employees e ON o.EmployeeID = e.EmployeeID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY o.OrderID, e.FirstName;
 
