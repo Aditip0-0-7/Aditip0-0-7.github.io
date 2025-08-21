a9414f012fe1ca95887fdc59667208de13d6431f
Aditip0-0-7 <aditipyomsriken@gmail.com>
2025-08-21 13:52:20 +0700

    Update Lad2.sql

--- Lad2.sql
+++ Lad2.sql
@@ -11,3 +11,20 @@ FROM Products as p JOIN Categories as c
 ON p.CategoryID = c.CategoryID
 WHERE CategoryName = 'seafood'
 
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID = Orders.Shipvia
+
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+AND OrderID = 10275
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.Shipvia
+WHERE OrderID=10275
