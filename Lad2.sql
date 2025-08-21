a583fb1a3d9fef54836eda16924dfecf714c2f36
Aditip0-0-7 <aditipyomsriken@gmail.com>
2025-08-21 13:45:40 +0700

    Update Lad2.sql

--- Lad2.sql
+++ Lad2.sql
@@ -1,3 +1,13 @@
 SELECT CategoryName, Productname, UnitPrice
 FROM Products, Categories
 WHERE Products.CategoryID = Categories.CategoryID
+
+SELECT c.Categoryname, p.Productname, p.UnitPrice
+FROM Products as p JOIN Categories as c 
+ON p.CategoryID = c.CategoryID  
+
+SELECT c.Categoryname, p.Productname, p.UnitPrice
+FROM Products as p JOIN Categories as c 
+ON p.CategoryID = c.CategoryID
+WHERE CategoryName = 'seafood'
+
