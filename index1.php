cf6321a665ac8009fb86042e98ec5baf07d2935a
Aditip0-0-7 <aditipyomsriken@gmail.com>
2025-12-04 09:00:12 +0700

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>php Array (เรียนวันที่ 4/12/25680)</title>
+</head>
+<body>
+    <?php
+
+    ?>
+</body>
+</html>
